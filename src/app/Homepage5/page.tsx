af6079a29cd7c8e9e7d3a25e75353883d82e62f7
ayyalraj <ayyalrajsundar@gmail.com>
2023-09-21 19:08:06 +0530

    add new folder

--- src/app/Homepage5/page.tsx
+++ src/app/Homepage5/page.tsx
@@ -1,12 +1,16 @@
-import Navbar from "../Components/Layout/Navbar";
+import Layout from "../Components/Layout/layout";
 import Achievements from "./Achievements";
+import Developer from "./Developer";
 import Hire from "./Hire";
+import Technologies from "./Technologies";
 export default function Home() {
     return (
       <>
-      <Navbar/>
+      <Layout/>
       <Hire/>
       <Achievements/>
+      <Technologies/>
+      <Developer/>
       </>
     )
 }
