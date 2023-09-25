3776ef7d1d51d776b5b7ed2c3c8604d89762f5d9
boobalan.r <boobalan.r@abserve.tech>
2023-09-25 14:49:08 +0530

    faq

--- src/app/Homepage4/Business.tsx
+++ src/app/Homepage4/Business.tsx
@@ -1,19 +1,25 @@
 import React from "react";
 import MyForm from "../Components/Reusable/form";
+
+
 export default function Business() {
+// const router =useRouter()
     return (
         <>
             <div className="mt-[145px] w-full flex flex-wrap md:flex-nowrap items-center bg-[#1E1E1E]">
                 <div className="md:w-1/2 flex p-2 justify-center mx-auto">
                     <div className="">
                         <p className="font-semibold text-[14px] text-[#005CFF] leading-normal">Get In Touch With Us</p>
-                        <p className="font-bold text-[#FFF] pt-[5px] pb-[20px] text-[28px] md:text-[30px] lg:text-[33px] md:max-w-[530px] leading-normal">Do You Want To Know More About Our Business?</p>
-                        <p className="text-[14px] text-[#fff] md:max-w-[590px] leading-[30px] ">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum
-                            has been the industry's standard dummy text ever since the 1500s.</p>
+                        <p className="text-[14px] text-[#fff] md:max-w-[590px] leading-[30px] ">
+                            {/* {navigation.pathname === "/help"
+                                ? "Do You Want To Know More Dedicated Developer Services?"
+                                : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."
+                            } */}
+                        </p>
                     </div>
                 </div>
                 <div className="w-full md:w-1/2  lg:p-[80px] mx-auto">
-                    <div className='bg-[#fff] rounded-md md:rounded-[20px]   mx-auto px-[58px] py-[58px]'>
+                    <div className='bg-[#fff] rounded-md md:rounded-[20px] mx-auto px-[58px] py-[58px]'>
                         <MyForm />
                     </div>
                 </div>
---
+++ src/app/help/assistance.tsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import styles from "../css/about.module.css"
+const Assistance = () => {
+    const cards = [
+        {
+          icon: "/assets/svg/target.svg",
+          title: "Office location",
+          location: "United States"
+        },
+        {
+          icon: "/assets/svg/cal.svg",
+          title: "Office location",
+          location: "United States"
+        },
+        {
+          icon: "/assets/svg/mail.svg",
+          title: "Office location",
+          location: "United States"
+        }
+      ];
+  return (
+    <>
+      <section className='relative '>
+        <img src="/assets/svg/spring.svg" className='absolute hidden md:block lg:left-[103px]' />
+        <div className='grid justify-items-center relative'>
+          <p className={`${styles.fadeDesign}`}>Get In Touch</p>
+          <p className='text-[35px] text-center font-bold relative leading-normal max-w-[326px]'>
+            We’re Here To Help You Any Time!
+            {[41, 81, 41].map((top, index) => (
+              <img
+                key={index}
+                src="/assets/image/fade.png"
+                className={`absolute top-[${top}px] hidden md:block ${index === 0 ? 'left-[-60px]' : index === 1 ? 'right-[-20px]' : 'md:right-[-126px] lg:right-[-225px]'
+                  }`}
+              />
+            ))}
+          </p>
+        </div>
+        <div className='mt-[70px] flex justify-center p-2 gap-y-10 flex-wrap mx-auto'>
+          {cards.map((card, index) => (
+            <div key={index} className='flex justify-center items-center h-full w-full sm:w-[338px] py-[54px] rounded-[21px] border m-auto shadow-sm'>
+              <div className='grid gap-[21px] relative'>
+                <img src="/assets/image/ellipse.png" className='mx-auto' alt='Ellipse' />
+                <img src={card.icon} className={`absolute top-[36px] left-[${index === 2 ? 41 : 46}px]`} alt='Icon' />
+                <p className='text-[18px] leading-[33px] font-medium text-center'>{card.title}</p>
+                <p className='text-[16px] font-semibold leading-[25px] text-center'>{card.location}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+
+      </section></>
+  )
+}
+
+export default Assistance
