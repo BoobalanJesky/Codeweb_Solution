0db62067c05c3f2805cae1335811afd3a397feb0
ayyalraj <ayyalrajsundar@gmail.com>
2023-09-27 12:56:43 +0530

    add new folder

--- src/app/Components/Layout/Navbar.tsx
+++ src/app/Components/Layout/Navbar.tsx
@@ -9,7 +9,7 @@ const Navbar = () => {
   return (
     <>
     {/* linearColor */}
-    <section className="relative pxPadNav  bg-default-white  mb-10 lg:mb-[115px]   flex justify-between gap-3  pt-[21px] items-center">
+    <div className="relative pxPadNav    mb-10 lg:mb-[115px]  background flex justify-between gap-3  pt-[21px] items-center">
         <div className={`${styles.NavbarItem} flex items-center lg:gap-[50px]`}>
          <img src="/assets/image/logo.png"/>
          <div>
@@ -49,7 +49,7 @@ const Navbar = () => {
               disabled={false}
               />
         </div> */}
-    </section>
+    </div>
     </>
   )
 }
